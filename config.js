c41e6f0f0aada74247d82722bda146793d607ad2
domzaj <domzaj7892@gmail.com>
2025-08-16 21:58:20 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "TWÓJ_API_KEY",
+  authDomain: "betpong-a2604.firebaseapp.com",
+  projectId: "betpong-a2604",
+  storageBucket: "betpong-a2604.firebasestorage.app",
+  messagingSenderId: "516021725514",
+  appId: "1:516021725514:web:6cdad8ad6f3f48f78a79b3",
+  measurementId: "G-LF0TYLD7H2"
+};
