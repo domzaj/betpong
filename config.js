6bbadc6fb7c8e8a1a156f619ab1d1358c5a2bbb6
domzaj <domzaj7892@gmail.com>
2025-08-16 22:23:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "TWÓJ_API_KEY",
+  apiKey: "AIzaSyAlPCV8QwmBICr-bNAM4z0W6WMcjFUp7cg",
   authDomain: "betpong-a2604.firebaseapp.com",
   projectId: "betpong-a2604",
-  storageBucket: "betpong-a2604.firebasestorage.app",
+  storageBucket: "betpong-a2604.appspot.com",   // poprawione
   messagingSenderId: "516021725514",
   appId: "1:516021725514:web:6cdad8ad6f3f48f78a79b3",
   measurementId: "G-LF0TYLD7H2"
